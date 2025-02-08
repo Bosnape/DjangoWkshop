c1e39a815325b4d583d5ff2beba38880d637f05c
Bosnape <pcabrejosm@gmail.com>
2025-02-08 17:04:33 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -9,12 +9,14 @@ def home(request):
     # return HttpResponse('<h1>Welcome to Home Page</h1>')
     # return render(request, 'home.html')
     # return render(request, 'home.html', {'name': 'Pablo Cabrejos'})
+    
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
         movies = Movie.objects.filter(title__icontains=searchTerm)
     else: 
         movies = Movie.objects.all()
     return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
 
 def about(request):
     # return HttpResponse('<h1>Welcome to About Page</h1>')
